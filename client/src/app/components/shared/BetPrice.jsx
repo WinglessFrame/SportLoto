274a2bfa9ba1f30e769b186ae49c42bc48cdae54
WinglessFrame <gleb122002@gmail.com>
2021-05-14 09:03:34 +0300

    price input styled

--- client/src/app/components/shared/BetPrice.jsx
+++ client/src/app/components/shared/BetPrice.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import '../../styles/BetPrice.scss'
+import arrowSVG from '../../images/down-chevron.svg'
 
 
 export default function BetPrice({ initialPrice, isEditable }) {
@@ -17,16 +18,38 @@ export default function BetPrice({ initialPrice, isEditable }) {
         }
     }
 
+    const arrowDownPressed = () => {
+        if (price > 1) {
+            setPrice(old => old - 1)
+        }
+    }
+
+    const arrowUpPressed = () => {
+        if (price < 5) {
+            setPrice(old => old + 1)
+        }
+    }
+
     return (
         <div className={`bet-price bet-price-color-${stylePicker(price)}`}>
             {!isEditable
                 ? `${price}`
-                : <input type='number' className="input-text bet-price--input"
-                    value={price}
-                    onChange={({ target }) => onChangeHandler(target)}
-                    min={1}
-                    max={5}
-                />
+                : <div className="input-wrapper">
+                    <input type='number' className="bet-price--input"
+                        value={price}
+                        onChange={({ target }) => onChangeHandler(target)}
+                        min={1}
+                        max={5}
+                    />
+                    <div className="price-buttons">
+                        <button className="price-btn price-btn-up" onClick={arrowUpPressed}>
+                            <img src={arrowSVG} alt="arrow up" />
+                        </button>
+                        <button className="price-btn price-btn-down" onClick={arrowDownPressed}>
+                            <img src={arrowSVG} alt="arrow up" />
+                        </button>
+                    </div>
+                </div>
             }
             $
             <span className='bet-price-txt'>Bet</span>
