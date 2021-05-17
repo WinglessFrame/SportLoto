6a6f41f0f292f7b499070c77820148d646d84a12
WinglessFrame <gleb122002@gmail.com>
2021-05-17 11:18:48 +0300

    Added Game model

--- server/main/models.py
+++ server/main/models.py
@@ -30,3 +30,20 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+class Game(models.Model):
+    user = models.ForeignKey(User, null=True, blank=False, verbose_name='games', on_delete=models.SET_NULL)
+    bet_price = models.PositiveIntegerField(null=False, blank=False, default=1)
+    bet = models.CharField(null=False, blank=False, max_length=100)
+    date = models.DateField(auto_now_add=True)
+    time = models.TimeField(auto_now_add=True)
+    result = models.BooleanField(null=False, blank=False)
+    matches = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'game {self.pk} {self.user.username}'
+
+    class Meta:
+        verbose_name = 'Game'
+        verbose_name_plural = 'Games'
